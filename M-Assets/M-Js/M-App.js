2ef63dbc90e2fe2fb72fb2105e2b21c87f1811ca
Meraz Meraz <devmeraz87@gmail.com>
2022-10-31 21:18:48 +0600

    first commit

---
+++ M-Assets/M-Js/M-App.js
@@ -0,0 +1,12 @@
+/**
+* @MevlanMeraj 10/31/2022
+* Code collected from Google search
+* licence MIT
+*/
+
+(() => {
+
+    const style = ["color: #fff", "background: #000", "padding: 8px 15px", "border-radius: 3px"].join(";");
+    console.log("%cMevlan Meraj 🖤💔 🥀", style);
+    
+})();
